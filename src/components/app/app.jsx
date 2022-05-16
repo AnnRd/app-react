905af7ed04b43504da15eedc98603647b4931359
Anna <anrd.rs@mail.ru>
2022-05-16 21:04:42 +0300

    employees-list component updated and employees-list-item inserted in it

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 
 import './app.css'; //тк webpack
 
@@ -14,6 +15,8 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
+
+            <EmployeesList/>
         </div>
     );
 }
